f1d26c05ca2e3fe3c061d2e6234f5bff22d860fb
thomasmars <thomasmars87@gmail.com>
2017-03-15 15:43:28 +0100

    Added some simple unit tests for verifying functionality of "most popular" filter
    HFP-536

--- test/search-service-spec.js
+++ test/search-service-spec.js
@@ -1,19 +1,45 @@
 import SearchService from '../src/scripts/search-service/search-service';
+import cacheMock from './mock/api/content-type-cache';
 
-describe('Search Service', () => {
-  // Check that attach is called
-  it('should return something', () => {
-    console.log('test');
+// describe('Search Service', () => {
+//   // Check that attach is called
+//   it('should return something', () => {
+//     console.log('test');
+//
+//     let service = new SearchService({
+//       apiRootUrl: 'test/mock/api/'
+//     });
+//
+//     let result = service.search('test')
+//       .then(result => console.log(JSON.stringify(result)));
+//   });
+//
+//   it('should return something else', () => {
+//     console.log('test2');
+//   });
+// });
 
-    let service = new SearchService({
-      apiRootUrl: 'test/mock/api/'
-    });
+describe('Search service filtering', () => {
+  const mockedService = {
+    contentTypes: () => {
+      return Promise.resolve(cacheMock.libraries);
+    }
+  };
+  const search = new SearchService(mockedService);
 
-    let result = service.search('test')
-      .then(result => console.log(JSON.stringify(result)));
+  it('should filter the most popular content type first (H5P.MultiChoice)', (done) => {
+    search.filter('popularity')
+      .then(cts => {
+        expect(cts[0].machineName).toEqual('H5P.MultiChoice');
+        done();
+      });
   });
 
-  it('should return something else', () => {
-    console.log('test2');
-  });
+  it ('should filter content types without popularity last', (done) => {
+    search.filter('popularity')
+      .then(cts => {
+        expect(cts[cts.length - 1].machineName).toEqual('H5P.Feedback');
+        done();
+      });
+  })
 });
