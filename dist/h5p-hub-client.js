16353c4439375e5d175afb3cf1b8cb713a2aaf72
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-03-21 14:09:56 +0100

    Add license info

--- dist/h5p-hub-client.js
+++ dist/h5p-hub-client.js
@@ -3025,13 +3025,19 @@ var ContentTypeDetailView = function () {
      * Sets the licence
      *
      * @param {string} type
+     * @param {string} owner
      */
 
   }, {
     key: "setLicence",
-    value: function setLicence(type) {
+    value: function setLicence(type, owner) {
       if (type) {
-        this.licencePanelBody.querySelector('.panel-body').innerText = type;
+        if (type === 'MIT') {
+          this.licencePanelBody.querySelector('.panel-body').innerHTML = "\n        <p>Copyright " + new Date().getFullYear() + " " + owner + "</p>\n\n        <p>Permission is hereby granted, free of charge, to any person obtaining a copy\n        of this software and associated documentation files (the \"Software\"), to deal\n        in the Software without restriction, including without limitation the rights\n        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n        copies of the Software, and to permit persons to whom the Software is\n        furnished to do so, subject to the following conditions:</p>\n        \n        <p>The above copyright notice and this permission notice shall be included in\n        all copies or substantial portions of the Software.</p>\n        \n        <p>THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE\n        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n        THE SOFTWARE.</p>\n        ";
+        } else {
+          this.licencePanelBody.querySelector('.panel-body').innerText = type;
+        }
+
         (0, _elements.show)(this.licencePanelHeading);
       }
 
@@ -3315,7 +3321,7 @@ var ContentTypeDetail = function () {
       this.view.setExample(contentType.example);
       this.view.setOwner(contentType.owner);
       this.view.setIsInstalled(contentType.installed);
-      this.view.setLicence(contentType.license);
+      this.view.setLicence(contentType.license, contentType.owner);
       this.view.setIsRestricted(contentType.restricted);
 
       // update carousel
