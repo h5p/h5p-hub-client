8a01a6f658d3079fe5fa6b142c7af02f31a2f565
Paal Joergensen <pal.jorgensen@gmail.com>
2017-03-23 10:54:57 +0100

    Minor fixes after review [JI-97]

--- src/scripts/utils/dictionary.js
+++ src/scripts/utils/dictionary.js
@@ -1,20 +1,29 @@
+/**
+ * Class responsible for providing translations
+ */
 export default class Dictionary {
 
+  /**
+   * Initialize the dictionary
+   *
+   * @param {Object} dictionary - dictionary as key/value
+   */
   static init(dictionary) {
     Dictionary.dictionary = dictionary;
   }
 
   /**
    * Get a string from the dictionary. Optionally replace variables
+   *
    * @param {string} key
    * @param {Object} replacements
    * @returns {string}
    */
   static get(key, replacements) {
-    var translation = Dictionary.dictionary[key];
+    let translation = Dictionary.dictionary[key];
 
     // Replace placeholder with variables.
-    for (var placeholder in replacements) {
+    for (let placeholder in replacements) {
       if (!replacements[placeholder]) {
         continue;
       }
