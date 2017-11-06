1747571bc4e0733860a57ab52eb6c934d1f52fe5
Frode Petterson <frode.andre.petterson@gmail.com>
2017-11-06 14:33:25 +0100

    HFP-530 Refactor list item into separate component

--- src/scripts/Components/TabPanel/Browse/List/List.js
+++ src/scripts/Components/TabPanel/Browse/List/List.js
@@ -1,8 +1,6 @@
 import React from 'react';
-
-import noIcon from '../../../../../images/content-type-placeholder.svg';
+import ListItem from './ListItem/ListItem';
 import Choose from '../../../Choose/Choose';
-import Dictionary from '../../../../utils/dictionary';
 
 class List extends React.Component {
   constructor(props) {
@@ -32,52 +30,10 @@ class List extends React.Component {
   }
 
   render() {
-    const listItems = this.props.contentTypes.map((contentType, i) => {
-      let id = contentType.machineName.toLocaleLowerCase().replace('.','-');
-      let tabindex = -1;
-      if (this.props.focused) {
-        if (this.props.focused === contentType) {
-          tabindex = 0; // Enable tabbing to selected item's button
-        }
-      }
-      else if (i === 0) {
-        tabindex = 0; // First item is selected but default, enabling tabbing
-      }
-      let title = (contentType.title || contentType.machineName);
-      let updateAvailable = (!contentType.isUpToDate && contentType.installed && contentType.canInstall);
-
-      return (
-        <li key={id} id={id} className="media">
-
-          <div className="media-left">
-            <img className="media-object" src={contentType.icon || noIcon} alt={title + ' ' + Dictionary.get('contentTypeIconAltText')}/>
-          </div>
-
-          <div className="media-body">
-            <div className="h4 media-heading">{title}</div>
-
-            {contentType.installed ? (
-              <button type="button" className="button button-primary" tabIndex={tabindex} onClick={event => this.handleUse(event, contentType)}>
-                <span></span>
-                {Dictionary.get('contentTypeUseButtonLabel')}
-              </button>
-            ) : (
-              <button type="button" className="button button-inverse-primary button-install'" tabIndex={tabindex}>
-                <span className="icon-arrow-thick"></span>
-                {Dictionary.get('contentTypeGetButtonLabel')}
-              </button>
-            )}
-
-            <div className={'content-type-update-info' + (updateAvailable ? '' : ' hidden')}>
-              {Dictionary.get('contentTypeUpdateAvailable')}
-            </div>
-
-            <div className="description">{contentType.summary || ''}</div>
-          </div>
-
-        </li>
-      );
-    });
+    const listItems = this.props.contentTypes.map((contentType, i) => (
+      <ListItem contentType={contentType}
+        tabindex={this.props.focused ? (this.props.focused === contentType ? 0 : -1) : (i === 0 ? 0 : -1)} />
+    ));
 
     return (
       <ol className="content-type-list" aria-hidden={!this.props.visible}>
---
+++ src/scripts/Components/TabPanel/Browse/List/ListItem/ListItem.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import noIcon from '../../../../../../images/content-type-placeholder.svg';
+import Dictionary from '../../../../../utils/dictionary';
+
+const ListItem = (contentType, tabIndex) => {
+
+  const id = contentType.machineName.toLocaleLowerCase().replace('.','-');
+  const title = (contentType.title || contentType.machineName);
+  const updateAvailable = (!contentType.isUpToDate && contentType.installed && contentType.canInstall);
+
+  return (
+    <li key={id} id={id} className="media">
+
+      <div className="media-left">
+        <img className="media-object" src={contentType.icon || noIcon} alt={title + ' ' + Dictionary.get('contentTypeIconAltText')}/>
+      </div>
+
+      <div className="media-body">
+        <div className="h4 media-heading">{title}</div>
+
+        {contentType.installed ? (
+          <button type="button" className="button button-primary" tabIndex={tabIndex} onClick={event => this.handleUse(event, contentType)}>
+            <span></span>
+            {Dictionary.get('contentTypeUseButtonLabel')}
+          </button>
+        ) : (
+          <button type="button" className="button button-inverse-primary button-install'" tabIndex={tabIndex}>
+            <span className="icon-arrow-thick"></span>
+            {Dictionary.get('contentTypeGetButtonLabel')}
+          </button>
+        )}
+
+        <div className={'content-type-update-info' + (updateAvailable ? '' : ' hidden')}>
+          {Dictionary.get('contentTypeUpdateAvailable')}
+        </div>
+
+        <div className="description">{contentType.summary || ''}</div>
+      </div>
+
+    </li>
+  );
+};
+
+ListItem.propTypes = {
+  contentType: PropTypes.object.isRequired,
+  tabIndex: PropTypes.number.isRequired
+};
+
+export default ListItem;
