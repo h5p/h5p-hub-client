412e2945e46ee845a3024f90d45b3ae813daccbe
timothyylim <timothylim23@gmail.com>
2017-11-14 10:50:01 +0100

    HFP-1640 Remove sdk styles, upload component

--- src/scripts/Components/TabPanel/UploadContent/UploadForm/UploadForm.js
+++ src/scripts/Components/TabPanel/UploadContent/UploadForm/UploadForm.js
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../UploadContent.scss';
+import './UploadForm.scss';
 import '../../../../utils/fetch';
 import Dictionary from '../../../../utils/dictionary';
 
