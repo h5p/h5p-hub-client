3ad0b1fd77e61efecf4b7764719162e9da0721f0
timothyylim <timothylim23@gmail.com>
2017-11-17 10:49:10 +0100

    HFP-1741 Remove unused js files

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -3,4 +3,3 @@ require('../../src/styles/main.scss');
 // Load library
 H5P = H5P || {};
 H5P.HubClient = require('../scripts/hub').default;
-H5P.HubServices = require('../scripts/hub-services').default;
