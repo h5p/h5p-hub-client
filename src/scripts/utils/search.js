54400e9b9e133cb8bf5763c6008a0b30e7373749
timothyylim <timothylim23@gmail.com>
2017-11-20 10:58:00 +0100

    HFP-1753 Do not order by installed
    Uninstalled content types should not be sent to the bottom
    of the result list.

--- src/scripts/utils/search.js
+++ src/scripts/utils/search.js
@@ -51,6 +51,7 @@ export default function search(list, filterBy, orderBy) {
       score: getSearchScore(filterBy, contentType)
     })).filter(result => result.score > 0 && !isUnavailable(result.contentType, list.apiVersion));
 
+    console.log(filtered);
     return multiSort(filtered);
   }
 
@@ -258,13 +259,7 @@ const sortOnProperty = (firstContentType, secondContentType, property, sortOrder
  * @return {number}
  */
 const sortSearchResults = (a,b) => {
-  if (!a.contentType.installed && b.contentType.installed) {
-    return 1;
-  }
-  if (a.contentType.installed && !b.contentType.installed) {
-    return -1;
-  }
-  else if (b.score !== a.score) {
+  if (b.score !== a.score) {
     return b.score - a.score;
   }
   else {
