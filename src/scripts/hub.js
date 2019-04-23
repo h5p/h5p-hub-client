2d5c685a95d2482a4e48bf3a98cfc301d1123249
Frode Petterson <frode.andre.petterson@gmail.com>
2019-04-23 15:03:02 +0200

    Add comment

--- src/scripts/hub.js
+++ src/scripts/hub.js
@@ -33,7 +33,7 @@ export default class HubClient {
           title={state.title}
           expanded={state.expanded}
           canPaste={state.canPaste}
-          contentId={parseInt(state.contentId)}
+          contentId={parseInt(state.contentId)} // TODO: contentId can be as string as well...
           contentTypes={state.contentTypes}
           selected={state.selected}
           getAjaxUrl={state.getAjaxUrl}
