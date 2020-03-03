bbc69637f4d4789807c5956bcd2fb6669c3ce0ea
Hannaes <hanna.solstad@joubel.com>
2020-03-03 16:02:10 +0100

    HFP-634 Fix eslint, NoContent more generic and make statless

--- src/scripts/Components/TabPanel/Reuse/NoContent/NoContent.js
+++ src/scripts/Components/TabPanel/Reuse/NoContent/NoContent.js
@@ -1,36 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './NoContent.scss';
-import Dictionary from '../../../../utils/dictionary'
-import noResults from '../../../../../images/no-results.svg'
+import Dictionary from '../../../../utils/dictionary';
+import noResults from '../../../../../images/no-results.svg';
 
-class NoContent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const NoContent = ({ headerText, suggestionText, tutorialUrl }) =>
+  (
+    <div className="no-content">
+      <img className="no-results-img" src={noResults} />
+      <section className="right-text">
+        <div className="header">
+          {headerText}
+        </div>
+        <div className="suggestion-text">
+          {suggestionText}
+        </div>
+        <div>
+          <a className="url" target="_blank" href={tutorialUrl}>{Dictionary.get('tutorials')}</a>
+        </div>
+      </section>
+    </div>
+  );
 
-  render() {
-    return (
-      <div className="no-content">
-        <img className="no-results-img" src={noResults}/>
-        <section className="right-text">
-            <div className="header">
-                {Dictionary.get('noContentHeader')} 
-            </div>
-            <div className="suggestion-text">
-                {Dictionary.get('noContentSuggestion')}
-            </div>
-            <div>
-              <a className="url" href={this.props.tutorialUrl}>{Dictionary.get('tutorials')}</a>
-            </div>
-        </section>
-      </div>
-    );
-  }
-}
 
 NoContent.propTypes = {
-    tutorialUrl: PropTypes.string
+  tutorialUrl: PropTypes.string
 };
+
 
 export default NoContent;
--- src/scripts/Components/TabPanel/Reuse/ReuseContent.js
+++ src/scripts/Components/TabPanel/Reuse/ReuseContent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NoContent from './NoContent/NoContent';
 //import PropTypes from 'prop-types';
 //import './ReuseContent.scss';
-//import Dictionary from '../../utils/dictionary';
+import Dictionary from '../../../utils/dictionary';
 
 class ReuseContent extends React.Component {
   constructor(props) {
@@ -14,10 +14,12 @@ class ReuseContent extends React.Component {
       <div className="reuse-view loaded">
         Reuse view
         <NoContent
-          tutorialUrl = "https://h5p.org/documentation/for-authors/tutorials"
-        />
+          tutorialUrl="https://h5p.org/documentation/for-authors/tutorials"
+          suggestionText={Dictionary.get('noContentSuggestion')}
+          headerText={Dictionary.get('noContentHeader')} />
       </div>
     );
+
   }
 }
 
