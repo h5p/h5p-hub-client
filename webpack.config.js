995ac0f602c8f4c955c6bde56e929425e80e2d76
Paal Joergensen <pal.jorgensen@gmail.com>
2020-03-05 14:29:48 +0100

    HFP-2941 Update NPMs

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,43 @@
-const webpack = require('webpack');
 const path = require('path');
-const isDevMode = JSON.parse(process.env.DEV_ENV || 0) == 1;
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-const extractSass = new ExtractTextPlugin({
-  filename: "h5p-hub-client.css"
-});
-
-const polyfillPromise = new webpack.ProvidePlugin({
-  'Promise': 'es6-promise',
-});
 
 const config = {
   entry: "./src/entries/dist.js",
-  devtool: isDevMode ? 'inline-source-map' : undefined,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: "h5p-hub-client.js",
-    sourceMapFilename: '[file].map'
-  },
-  resolve: {
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
         use: 'babel-loader'
       },
       {
         test: /\.scss$/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: "css-loader",
-              options: {
-                minimize: !isDevMode
-              }
-            },
-            {
-              loader: "resolve-url-loader"
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true
-              }
-            }
-          ],
-
-          fallback: "style-loader"
-        })
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(svg)$/,
-        include: path.join(__dirname, 'src/images'),
-        loader: 'url-loader?limit=20000'
-      } // inline base64 URLs for <=10k images, direct URLs for the rest
+        include: path.resolve(__dirname, 'src'),
+        loader: 'url-loader?limit=1000000'
+      }
     ]
-  },
-  plugins: [
-    extractSass,
-    polyfillPromise
-  ]
+  }
 };
 
-if (!isDevMode) {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  }));
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
 
-  config.plugins.push(new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production')
-  }));
-}
-
-module.exports = config;
+  return config;
+};
