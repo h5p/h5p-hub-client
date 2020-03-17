b724037a96217fb15c344e17a087ffd325f6ae36
Paal Joergensen <pal.jorgensen@gmail.com>
2020-03-17 09:32:16 +0100

    HFP-694 Fix wrong word casing for scss import

--- src/scripts/Components/CheckboxList/CheckboxList.js
+++ src/scripts/Components/CheckboxList/CheckboxList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './CheckBoxList.scss';
+import './CheckboxList.scss';
 import Checkbox from '../Checkbox/Checkbox';
 
 const CheckboxList = ({ items, onChecked, checked, filter }) => {
