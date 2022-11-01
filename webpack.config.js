b047c101523ad42eb4987b2c27d671e2272b6398
Frode Petterson <frode.andre.petterson@gmail.com>
2022-11-01 10:38:43 +0100

    Fix webpack build chain to be more standardized

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,73 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const nodeEnv = process.env.NODE_ENV || 'development';
-const libraryName = process.env.npm_package_name;
-
-const config = {
-  mode: nodeEnv,
-  context: path.resolve(__dirname, 'src'),
-  entry: "./entries/dist.js",
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: `${libraryName}.js`,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: [
-          path.resolve(__dirname, 'node_modules/react-async'),
-          path.resolve(__dirname, 'src')
-        ],
-        use: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(svg)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'url-loader',
-        options: {
-          limit: 1000000
-        }
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${libraryName}.css`
-    })
-  ]
-};
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
+  const devMode = argv.mode !== "production";
+  const config = {
+    context: path.resolve(__dirname, 'src'),
+    entry: "./entries/dist.js",
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "h5p-hub-client.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: [
+            path.resolve(__dirname, 'node_modules/react-async'),
+            path.resolve(__dirname, 'src')
+          ],
+          use: 'babel-loader'
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 1_000_000,
+              },
+            }
+          ],
+        }
+      ]
+    },
+    plugins: []
+  };
+  if (devMode) {
     config.devtool = 'inline-source-map';
     if (argv.api !== 'test') {
-      config.entry = "./src/entries/dev.js";
+      config.entry = "./entries/dev.js";
     }
   }
-
+  else {
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: "h5p-hub-client.css"
+    }));
+  }
   return config;
 };
